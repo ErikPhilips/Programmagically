87ca2e64b0ad16b6b5a779e1b908e4b5ac30236a
ErikPhilips <erikphilips@gmail.com>
2017-11-16 15:19:43 -0800

    Update DocWriteFixup.js

--- DocWriteFixup.js
+++ DocWriteFixup.js
@@ -7,13 +7,13 @@ var _$_write= document.write;
     var $script = $(selector).filter((i,e)=>{ return !$(e).parent().is('head');});
     var $wrapper = $script.data('js-script-tag-wrapper');
     if ($wrapper === undefined){
-      $wrapper = $('<div></div>');
+      $wrapper = $('<div class="js-wrapper"></div>');
       $script.data('js-script-tag-wrapper', $wrapper);
       $script.wrap($wrapper);
     }
     $wrapper.append("<div>Start Content</div>");
-    $wrapper.append(content);
-    console.log(content);
-    $wrapper.append("<div>End Content</div>");
+    //$wrapper.append(content);
+    //console.log(content);
+    //$wrapper.append("<div>End Content</div>");
   };
 console.log("DocWriteFixup.js::end");
