e59200cbc2a31bc721cd00717d58eb6dd3fca982
ErikPhilips <erikphilips@gmail.com>
2017-11-16 14:01:47 -0800

    Update DocWriteFixup.js

--- DocWriteFixup.js
+++ DocWriteFixup.js
@@ -3,13 +3,20 @@ var _$_write= document.write;
   document.write = (content) => {
     //console.log(document.currentScript);
     var $script = $(document.currentScript);
+    var src = $script[0].prop("src");
+    console.log(src);
     var $parent = $script.parent();
     console.log($parent);
+    var selector = "script[src='" + src + "']");
+    console.log(selector);
+    var $script2 = $(selector);
+    console.log($script2);
 
     //console.log(content);
     //var $content = $(content);
 
     //$content.insertAfter($script);
-    $(content).insertAfter($script);
+    //$(content).insertAfter($script);
+    console.log("-------------------------------------");
   };
 console.log("DocWriteFixup.js::end");
